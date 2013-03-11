71baa7b7be557bca12ed8b6eb0a078fd732cfaa8
Aaron Brongersma <aaron.brongersma@gmail.com>
2013-03-10 22:33:36 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intervene::Application.config.secret_token = '64204cd9a3739f2aa384ceef3eb8d5374a01c22f201a420793ace43b2b1131594df68a9bbd4afdf907b0f147a3965f98621b94df2a8a8f8ecf08ed965fb9a50a'
